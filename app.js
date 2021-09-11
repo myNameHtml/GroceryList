5ff3394d45292b08d1abd099721ccb35a8e3ff04
Kyaw Htet Htun <kyawhtethtun65@.com>
2021-09-12 01:11:54 +0200

    Update: Searchbar, Delete Button, Display Item

--- app.js
+++ app.js
@@ -1,55 +1,40 @@
-function Submit(){
-    AddProductAndAmount();
-    addElement_Br();
-}
+const product = document.getElementById('product');
+const list = document.getElementById('list');
+const btnSubmit = document.getElementById('submitButton');
 
-function addElement_Br() {
-    // create a new div element
-    const newDiv = document.createElement("br");
-  
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-}
-
-function AddProductAndAmount(){
-    // only if content are in inputs
-    if (document.getElementById('product').value && document.getElementById('amount').value) {
-        
-        // Create span
-        var aProduct = document.createElement('span');
-        var aAmount = document.createElement('span');
-
-        // Add text to span
-        var linkTextProduct = document.createTextNode(document.getElementById('product').value+" ");
-        var linkTextAmount = document.createTextNode(document.getElementById('amount').value+" ");
-        
-        // Create Div
-        const newDiv = document.createElement("div");
-        const currentDiv = document.getElementById("div");
-        newDiv.id = "result";
-        document.body.insertBefore(newDiv, currentDiv);
-
-        // Append product span to div
-        aProduct.appendChild(linkTextProduct);
-        aProduct.className = "product";
-        newDiv.appendChild(aProduct);   
-
-        // Append amount span to div
-        aAmount.appendChild(linkTextAmount);
-        aAmount.className = "amount";
-        newDiv.appendChild(aAmount);
-
-        // Create Button
-        let btn = document.createElement('button');
-        btn.innerHTML = "<strong>"+"CHECK"+"</strong>";
-        btn.className = "checked-button";
-        btn.id = "check-btn";
-        newDiv.appendChild(btn);
+product.addEventListener("keypress", (e)=>{
+    if (e.key === 'Enter' && product.value) {
+        AddItems();
     }
+})
 
-    document.getElementById("check-btn").addEventListener("click", function(){
-        const checkedDiv = document.getElementById('result');
-        checkedDiv.remove();
-    });
+btnSubmit.addEventListener("click", () =>{
+    // only if content are in inputs
+    if (product.value) {
+        AddItems();
+    }
+})
+
+function AddItems(){
+    // Create div
+    const newDiv = document.createElement("div");
+    newDiv.id = "items-container";
+    list.appendChild(newDiv);
+
+    // Create paragraph for product
+    var pTag = document.createElement('p');
+    pTag.innerText = product.value;
+    pTag.className = "list-font";
+    newDiv.appendChild(pTag);
+    product.value = "";
+
+    // Create button for removing item
+    var btn = document.createElement("button");
+    btn.innerText = "DELETE"
+    btn.className = "checked-button";
+    newDiv.appendChild(btn);
+
+    btn.addEventListener("click", () =>{
+        newDiv.remove(newDiv);
+    })
 }
